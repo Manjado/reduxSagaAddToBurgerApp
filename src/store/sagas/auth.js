4da06d22eb864964cebfa6a5c8dedd3eb3f9372f
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-06-22 17:19:52 +0200

    Handling Authentication qith a Saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,11 +1,45 @@
+import { delay } from 'redux-saga';
 import { put } from 'redux-saga/effects';
-import * as actionTypes from '../actions/actionTypes';
+import axios from 'axios';
+
+import * as actions from '../actions/index'
+
 
 export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
-    yield put({
-        type: actionTypes.AUTH_LOGOUT
-    })
+    yield put(actions.logoutSucceed());
 }
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime * 1000);
+    yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+    yield put(actions.authStart());
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCpiQtTD024k1H3iPUYkd0ix3EhZXBIVM0';
+    if(!action.isSignup) {
+        url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCpiQtTD024k1H3iPUYkd0ix3EhZXBIVM0'
+    }
+    axios.post(url, authData)
+        .then(response => {
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('expirationDate', expirationDate);
+            localStorage.setItem('userId', response.data.localId);
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
+        })
+        .catch(err => {
+            dispatch(authFail(err.response.data.error));
+        })
+}
+
+//252
